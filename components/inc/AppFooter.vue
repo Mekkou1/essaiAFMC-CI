d0b7a13c4a39bdeb81cfb25dd1394a2bc4249235
Mekkou1 <147875512+Mekkou1@users.noreply.github.com>
2024-10-21 12:00:23 +0000

    Ajout du dossier dist

--- components/inc/AppFooter.vue
+++ components/inc/AppFooter.vue
@@ -68,7 +68,7 @@
                   <img src="images/linkdin.png" class="fab fa-linkedin-in" alt="linkedin" style="width: 40px; height: 40px;">
                 </a>
               </div>
-              <p>© 2024 . Tous droits réservés. Réalisé par: <a href="https://www.facebook.com/levelprod001?mibextid=LQQJ4d">Levelprod</a> / Développé par: <a href="https://www.techartisan./">TechArtisan</a></p>
+              <p>© 2024 . Tous droits réservés. Réalisé par: <a href="https://www.facebook.com/levelprod001?mibextid=LQQJ4d">Levelprod</a> / Développé par: <a href="https://www.facebook.com/profile.php?id=61565538052691&mibextid=ZbWKwL">TechArtisan</a></p>
             </div>
           </div>
         </div>
